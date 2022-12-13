722ec2c73326badd0b9be9ad606b621503bccd17
nikitalita <69168929+nikitalita@users.noreply.github.com>
2022-12-13 11:20:56 -0800

    test compile profiling on CI

--- modules/text_server_adv/text_server_adv.cpp
+++ modules/text_server_adv/text_server_adv.cpp
@@ -27,7 +27,7 @@
 /* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
-
+// test
 #include "text_server_adv.h"
 
 #ifdef GDEXTENSION
