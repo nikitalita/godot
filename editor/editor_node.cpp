04e4478aeb27c10e85f13423aefbeb69ca05c48f
Zhen Luo <461652354@qq.com>
2026-02-27 20:12:54 +0800

    Fix `reload_scene_from_path` may crash

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -4477,6 +4477,7 @@ void EditorNode::_remove_edited_scene(bool p_change_tab) {
 	// When scene gets closed no node is edited anymore, so make sure the editors are notified before nodes are freed.
 	hide_unused_editors(SceneTreeDock::get_singleton());
 	SceneTreeDock::get_singleton()->clear_previous_node_selection();
+	SceneTreeDock::get_singleton()->set_selected(nullptr);
 
 	int new_index = editor_data.get_edited_scene();
 	int old_index = new_index;
